cf4ce0bceeee926b425c2ddd711ef99f1f7b9ec4
jackThammer <joelulahanna@gmail.com>
2018-07-18 18:04:06 -0600

    add readme and few testing

--- consumerApp/dna/needs_backup/needs_backup.js
+++ consumerApp/dna/needs_backup/needs_backup.js
@@ -65,12 +65,6 @@ function backupAll() {
  */
 function genesis () {
 //Testing
-  var  profileEntry={"age":100,"name":"Zo-el","tags":["Thats my name"]}
-  var   profileString="Sakura"
-
-  commit("profile", profileEntry);
-  commit("profile_string", profileString);
-  commit("profile_string", "Saske");
   return true;
 }
 
@@ -86,15 +80,6 @@ function bridgeGenesis(side, dna, appData) {
 //  Validation functions for every change to the local chain or DHT
 // -----------------------------------------------------------------
 
-/**
- * Called to validate any changes to the local chain or DHT
- * @param {string} entryName - the type of entry
- * @param {*} entry - the entry data to be set
- * @param {object} header - header for the entry containing properties EntryLink, Time, and Type
- * @param {*} pkg - the extra data provided by the validate[X]Pkg methods
- * @param {object} sources - an array of strings containing the keys of any authors of this entry
- * @return {boolean} is valid?
- */
 function validateCommit (entryName, entry, header, pkg, sources) {
   // debug("entry_type:"+entryName+"entry"+JSON.stringify(entry)+"header"+JSON.stringify(header)+"PKG: "+JSON.stringify(pkg)+"sources"+sources);
 
@@ -103,20 +88,20 @@ function validateCommit (entryName, entry, header, pkg, sources) {
 
   if (validate(entryName, entry, header, pkg, sources)) {
     if (backupEnabled) {
+      debug("consumerApp: Calling backup App!");
       var backup_commit = {
-        sourceAppDNA:App.DNA.Hash,
-        header: {
-          type : entryName,
-          sig : JSON.parse(header.Sig.replace(/ /g,',').replace(/['{''}']/g, '')),
-          hash : makeHash(entryName,entry),
-          time : header.Time,
-          nextHeader : header.NextHeader,
-          next : entryName+" :"+header.Next,
-          entry : header.EntryLink,
-        },
-        content:entry,
+      sourceAppDNA:App.DNA.Hash,
+      header: {
+        type : entryName,
+        sig : JSON.parse(header.Sig.replace(/ /g,',').replace(/['{''}']/g, '')),
+        hash : makeHash(entryName,entry),
+        time : header.Time,
+        nextHeader : header.NextHeader,
+        next : entryName+" :"+header.Next,
+        entry : header.EntryLink,
+      },
+      content:entry,
       }
-      debug("consumerApp: Calling backup App!")
       bridge(getBackupApps()[0].CalleeApp, 'backupChain', 'backup', backup_commit);
     }
     return true;
@@ -127,167 +112,71 @@ function validateCommit (entryName, entry, header, pkg, sources) {
 function validate(entryName, entry, header, pkg, sources){
   switch (entryName) {
     case "profile":
-      //debug("%header");
-      debug(parseInt(sign(JSON.stringify(entry))));
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return true;
     case "profile_string":
-      debug(sign(entry));
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return true;
     default:
-      // invalid entry name
       return false;
   }
 }
 
-/**
- * Called to validate any changes to the local chain or DHT
- * @param {string} entryName - the type of entry
- * @param {*} entry - the entry data to be set
- * @param {object} header - header for the entry containing properties EntryLink, Time, and Type
- * @param {*} pkg - the extra data provided by the validate[X]Pkg methods
- * @param {object} sources - an array of strings containing the keys of any authors of this entry
- * @return {boolean} is valid?
- */
 function validatePut (entryName, entry, header, pkg, sources) {
   switch (entryName) {
     case "profile":
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return false;
     case "profile_string":
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return false;
     default:
-      // invalid entry name
       return false;
   }
 }
 
-/**
- * Called to validate any changes to the local chain or DHT
- * @param {string} entryName - the type of entry
- * @param {*} entry - the entry data to be set
- * @param {object} header - header for the entry containing properties EntryLink, Time, and Type
- * @param {string} replaces - the hash for the entry being updated
- * @param {*} pkg - the extra data provided by the validate[X]Pkg methods
- * @param {object} sources - an array of strings containing the keys of any authors of this entry
- * @return {boolean} is valid?
- */
+
 function validateMod (entryName, entry, header, replaces, pkg, sources) {
   switch (entryName) {
     case "profile":
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return false;
     case "profile_string":
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return false;
     default:
-      // invalid entry name
       return false;
   }
 }
 
-/**
- * Called to validate any changes to the local chain or DHT
- * @param {string} entryName - the type of entry
- * @param {string} hash - the hash of the entry to remove
- * @param {*} pkg - the extra data provided by the validate[X]Pkg methods
- * @param {object} sources - an array of strings containing the keys of any authors of this entry
- * @return {boolean} is valid?
- */
 function validateDel (entryName, hash, pkg, sources) {
   switch (entryName) {
     case "profile":
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return false;
     case "profile_string":
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return false;
     default:
-      // invalid entry name
       return false;
   }
 }
 
-/**
- * Called to validate any changes to the local chain or DHT
- * @param {string} entryName - the type of entry
- * @param {string} baseHash - the hash of the base entry being linked
- * @param {?} links - ?
- * @param {*} pkg - the extra data provided by the validate[X]Pkg methods
- * @param {object} sources - an array of strings containing the keys of any authors of this entry
- * @return {boolean} is valid?
- */
 function validateLink (entryName, baseHash, links, pkg, sources) {
   switch (entryName) {
     case "profile":
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
-
-
-
       return false;
     case "profile_string":
-      // be sure to consider many edge cases for validating
-      // do not just flip this to true without considering what that means
-      // the action will ONLY be successfull if this returns true, so watch out!
       return false;
     default:
-      // invalid entry name
       return false;
   }
 }
 
-/**
- * Called to get the data needed to validate
- * @param {string} entryName - the name of entry to validate
- * @return {*} the data required for validation
- */
 function validatePutPkg (entryName) {
   return null;
 }
 
-/**
- * Called to get the data needed to validate
- * @param {string} entryName - the name of entry to validate
- * @return {*} the data required for validation
- */
 function validateModPkg (entryName) {
   return null;
 }
 
-/**
- * Called to get the data needed to validate
- * @param {string} entryName - the name of entry to validate
- * @return {*} the data required for validation
- */
 function validateDelPkg (entryName) {
   return null;
 }
 
-/**
- * Called to get the data needed to validate
- * @param {string} entryName - the name of entry to validate
- * @return {*} the data required for validation
- */
 function validateLinkPkg (entryName) {
   return null;
 }
